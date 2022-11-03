580e120c2425115039d0d743924e9618d693fe75
Bikramjit Singh <singhbikramjit222@gmail.com>
2022-11-03 15:39:58 -0700

    Made first assiciations

--- practice-for-ch-sql-associations-long-practice/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/enrollment.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    belongs_to :course, 
+    primary_key: :id,
+    class_name: :Course, 
+    foreign_key: :course_id
+
+    belongs_to :student,
+    primary_key: :id,
+    class_name: :User,
+    foreign_key: :student_id
+
 end
